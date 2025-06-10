565376ac468dd744290e1c4b81ccf5b4c5c0dcf0
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-10 16:24:56 +0530

    resolve cors error

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+app.options("/api/google", cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
