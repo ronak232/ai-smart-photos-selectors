0037552d3366a6bd30c0c9955ceaab88f80c434f
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-10 18:32:44 +0530

    resolve cors error

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -6,9 +6,12 @@ import { google } from "googleapis";
 export const googleAuthHandler = async (req, res) => {
   try {
     const { code } = req.body;
+    if (!code) return res.status(400).json({ message: "Missing code" });
+
     const { tokens } = await oauth2Client.getToken({
       code,
-      redirect_uri: "https://ronak232.github.io/PostPal-Intelligent-Photo-Selector/",
+      redirect_uri:
+        "https://ronak232.github.io/PostPal-Intelligent-Photo-Selector/",
     });
 
     oauth2Client.setCredentials(tokens);
@@ -17,32 +20,32 @@ export const googleAuthHandler = async (req, res) => {
       auth: oauth2Client,
       version: "v2",
     });
-    const userInfo = await oauth2.userinfo.get();
 
+    const userInfo = await oauth2.userinfo.get();
     const { email, name, picture } = userInfo.data;
 
     let user = await User.findOne({ email });
-
     if (!user) {
-      user = User.create({
+      user = await User.create({
         userName: name,
         email,
         image: picture,
       });
     }
+
     const { _id } = user;
+
     const getToken = jwt.sign({ _id, email }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
+
     return res.status(200).json({
-      sucess: true,
+      success: true,
       getToken,
       user,
     });
   } catch (error) {
-    console.error("google error ", error.message);
-    res.status(500).json({
-      message: "internal server error",
-    });
+    console.error("google error", error);
+    res.status(500).json({ message: "internal server error" });
   }
 };
