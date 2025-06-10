9bea81fcc42e07a2691843bbb5696933316ecff5
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-10 20:10:35 +0530

    cors error fix for google auth

--- server/server.js
+++ server/server.js
@@ -10,13 +10,19 @@ import { connectDB } from "./database/db.js";
 const app = express();
 const server = createServer(app);
 app.use(express.json());
-routes.options('/', cors());
 app.use(bodyParser.json());
 await connectDB();
-const allowedOrigins = ["http://localhost:5173", "https://ronak232.github.io/*"];
+const allowedOrigins = ["http://localhost:5173", "https://ronak232.github.io"];
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
