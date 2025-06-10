6944be479c009e64266fb96f0decf425bd3a6083
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-10 13:01:37 +0530

    fix error en variable undefined

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
     cors: {
-      origin: import.meta.env.VITE_BASEURL,
+      origin: "*",
     },
   },
 });
