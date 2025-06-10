5397122f1fd5cd64e87ae45c3cb9b2362cbdac06
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-10 11:05:52 +0530

    fix mismatch uri backend

--- server/configs/google-auth.js
+++ server/configs/google-auth.js
@@ -3,5 +3,5 @@ import { google } from "googleapis";
 export const oauth2Client = new google.auth.OAuth2({
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirectUri: "https://ronak232.github.io/PostPal-Intelligent-Photo-Selector/",
+  redirectUri: "https://ronak232.github.io",
 });
