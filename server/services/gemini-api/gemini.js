0cfe00ef9c5ec677555b067fb35ae59a43a44d97
Ronak Mathur <ronakmathur9@gmail.com>
2025-06-11 15:28:31 +0530

    refactor the gemini prompt for more natural response

--- server/services/gemini-api/gemini.js
+++ server/services/gemini-api/gemini.js
@@ -35,26 +35,23 @@ export class GeminiModelInit {
         let userOptions = "";
 
         if (userPrompt && userPrompt.length > 0) {
-          userOptions += ` You are my good friend who always help best images extact the occasion and event from ${userPrompt}? and also help me choose to best photos for posting for instagram, facebook, whatapp and other social media...
-          Analyze the facial expressions, background, and mood of each image based on the analysis of the images provide by user best images or image for the occasion and relatio.
-          You need to analyse each images deeply based on what userprompt - ${userPrompt}.
-          - A short visual preview for each image (use <img src="..."> tags with fixed height and center of div)
+          userOptions += `When you'll get user prompt understand and extract important occasion and event from ${userPrompt}? and also help to choose the best photos for posting on instagram, facebook, whatsapp, other social media or any occasion...
+          Analyze the facial expressions, background, and mood from images and based on the analysis, provide best images or image to the user.
+          You need to analyse each image deeply based on what user asked - ${userPrompt}.
+          - Add short visual preview of those image/images you analysed for best (use <img src="..."> tags with fixed height and center of div)
        `;
         } else {
-          userOptions += ` You are my good friend who always help best images for the ${occasion}? and also  help me choose to best photos for posting for instagram, facebook, whatapp and other social media...
-          Analyze the facial expressions, background, and mood of each image based on the analysis of the images provide by user best images or image for the ${occasion} and relation ${relatedTo}.
+          userOptions += ` When you'll get ${occasion}? and ${relatedTo} value, help user to choose best photos for posting for instagram, facebook, whatsapp, other social media or any occasion..
+          Analyze the facial expressions, background, mood, occasion, event of each image then provide options from images to user.
           You can assume each image has ${relatedTo} or ${occasion} property.
-          - A short visual preview for each image (use <img src="..."> tags with fixed height)
+          - Add short visual preview of those image/images you analysed for best (use <img src="..."> tags with fixed height and center of div)
        `;
         }
         return userOptions;
       }
       let userChoice = userOption();
 
-      const contentParts = createUserContent([
-        ...parts,
-        userChoice,
-      ]);
+      const contentParts = createUserContent([...parts, userChoice]);
 
       const response = await this.gemini.models.generateContent({
         model: this.model,
@@ -66,21 +63,22 @@ export class GeminiModelInit {
               threshold: "2",
             },
           ],
-          systemInstruction: `Please return the response in a well-formatted HTML responsive grid layout like using heading paragraph bold list tag**, styled using **Tailwind CSS** and add custom class name for styling like best-img with name on each img tag. 
+          systemInstruction: `You are like my good friend Please return the response in a well-formatted HTML responsive grid layout on each devices, use heading paragraph bold list tag**, styled using **Tailwind CSS** and add custom class name for styling like best-img with name on each img tag. 
           And in end write the about best image in a bold tag.
           Make it visually modern, elegant, and clean. Use a responsive card layout.
           Make sure to use the **Tailwind CSS** classes for styling, and ensure the layout is responsive and visually appealing.
           Remove Doctype, head, meta, title or body tags and other unnecessary tags just wrap in div tag
           Remove redundant technical jargons like tailwind, html,css
           return in html format
-          Rules for the response
-          - label for each image like (Best Images or Best Image)
+          Rules for the response:
+          - label for each image like (if multiple good images then name it like(Best-Image-Name) or if only single image name it (Best-image)) 
           - keep in mind you have to smartly provide best images not every provided images.
-          For example - If user provide two images return only best one of them 
+          For example - If user provide two images return only best one... 
           Similarly If user provide multiple images ${
             imagesList.length >= 3
           } then compare the images return two best images. and so on.
           - Do not show preview of the uploaded images...
+
           `,
         },
       });
